32ba07fa992e10902792d96ed76ea3dc4a93ae2b
Marcel Garus <marcel.garus@gmail.com>
2020-05-08 13:59:15 +0200

    add example

---
+++ example/main.dart
@@ -0,0 +1,8 @@
+import 'package:todo/todo.dart';
+
+int maximumOf(List<int> numbers) {
+  if (numbers.isEmpty) {
+    return TODO("Handle empty list of numbers.");
+  }
+  TODO("Implement finding the maximum.");
+}
